47fa2d25de1a4df9bbcabe8c74d558e5161ce89f
Xondamir0 <alimjanovxondamir01@gmail.com>
2025-01-08 00:09:19 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,67 @@
+// console.log(Math.floor(num));
+// console.log(Math);
+
+
+// const operation = +prompt("1) yoz \n 2) kuz \n 3) qish \n 4) bahor  ")
+
+// if(operation == 1){
+//     alert("Iltimos yuqaroq kiyinib yuring");
+// }else if(operation == 2){
+//     alert ("Iltimos qalin kurtka kiyib yuring")
+// }else if(operation == 3){
+//     alert("Iltimos issiq kiyim kiying")
+// }else if(operation == 4){
+//     alert("Iltimos ozingiz bilan zontik olib yuring")}
+// else{
+//     alert("Kechirasiz, 1-4 raqamni tanlang!")
+// }
+
+// const randomraqam = Math.round(Math.random() *10)
+// const random = +prompt("1 dan 10 gacha son kiriting:")
+
+// if ( randomraqam === random){
+//     alert("Tabriklaymiz siz to'gri topdingiz")
+// } else {
+//     alert(`Afsus,no'to'gri. To'gri javob:${randomraqam}`)
+// }
+
+
+// const savol =alert("Hozirda sizda random son chiqadi")
+
+// const randomRaqam = Math.round(Math.random() * 100);
+
+// const multiplied = randomRaqam * 2;
+
+// const divided = Math.floor(randomRaqam / 2);
+
+
+// alert(`Random chiqqan son :${randomRaqam * 2}`)
+
+// const agree = confirm("Biz chiqqan sonni 2 ga kopaytirmoqchi va bo'lmoqchimiz.")
+
+// const randomraq = Math.round(Math.random())
+// alert(`Random orqali chiqqan son: ${randomRaqam}\nRandom chiqqan sonni boldik: ${divided}\nRandom chiqqan sonni kopaytirdik:${multiplied}`)
+
+
+// const javob = confirm("Ha yoki yoqni bosing")
+    // alert(ism.length);
+    
+    
+    const ism = prompt("Ismingizni kiriting")
+    const harf = prompt("Bironta harf kiriting")
+
+    // if(ism == ""){
+    //    alert("Iltimos ismingizni kiriting");
+    // }
+
+  if(ism.includes(harf)){
+        alert(`Ismingizda ${harf} harfi bor`)
+    }
+
+        else{
+    alert(`Ismingizda ${harf} harf yo'q`)}
+
+
+
+
+
